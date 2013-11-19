7f17b4137b9e696af71655d9ab90206fc78892a5
Cabris <christtabris@gmail.com>
2013-11-20 04:01:12 +0800

    prototye
    4.2

---
+++ Assets/resources/Dragable.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class Dragable : MonoBehaviour
+{
+	public bool isMouseDown;
+	public bool enableMove;
+	public bool isFixOffset{ get; set; }
+
+	public Vector2 fixOffSet = new Vector2 ();
+	private Camera cam;
+	private Transform myTransform  ;
+	private Vector3 position;
+	Vector3 offSet;
+	Vector3 mousePos ;
+	
+	void Start ()
+	{
+		isMouseDown = false;
+		myTransform = this.transform;
+		position = myTransform.position;
+		offSet = Vector3.zero;
+		isFixOffset = false;
+		cam = Camera.main;
+		if (!cam) {
+			Debug.LogError ("Can't find camera tagged MainCamera");
+			return;
+		}
+	}
+
+	void OnMouseDown ()
+	{
+		isMouseDown = true;
+		position = myTransform.position;
+		offSet = position - cam.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, 0));
+		offSet.z = 0;
+	}
+ 
+	void OnMouseUp ()
+	{	
+		isMouseDown = false;	
+	}
+	
+	void FixedUpdate ()
+	{
+		rigidbody.isKinematic=isMouseDown;
+		mousePos = Input.mousePosition;
+		Vector3 mousePos_scene = cam.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, 0));
+		mousePos_scene.z = 0;
+		Vector3 move = mousePos_scene - position;
+		move.z = 0;
+		move += offSet;
+		if (isMouseDown && enableMove) {
+			if (isFixOffset)
+				myTransform.position = mousePos_scene + new Vector3 (fixOffSet.x, fixOffSet.y, 0);
+			else
+				myTransform.position = position + move;
+		}
+	}
+}
---
+++ Assets/resources/Hand.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour
+{
+	float spring;
+	float damper;
+	RigidbodyConstraints free, locked;
+	Uni2DSprite sprite;
+	Color32 linkColor, freeColor;
+	bool isLock;
+	Transform bodyT;
+	// Use this for initialization
+	void Start ()
+	{
+		free = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+		locked = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+		sprite = this.GetComponent<Uni2DSprite> ();	
+		linkColor = new Color32 (255, 255, 255, 255);
+		freeColor = new Color32 (255, 255, 255, 128);
+		IsLink = true;
+		isLock = false;
+		bodyT=this.GetComponent<SpringJoint> ().connectedBody.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		if (IsLink) {
+			this.GetComponent<SpringJoint> ().spring = this.spring;
+			this.GetComponent<SpringJoint> ().damper = this.damper;
+			
+			Color32 c=sprite.VertexColor;
+			c.a=linkColor.a;
+			sprite.VertexColor=c;
+		} else {
+			this.GetComponent<SpringJoint> ().spring = 0;
+			this.GetComponent<SpringJoint> ().damper = 0;
+			Color32 c=sprite.VertexColor;
+			c.a=freeColor.a;
+			sprite.VertexColor=c;
+		}
+	}
+	
+	public void LockHand (Vector3 pos)
+	{
+		Vector3 _pos = transform.position;
+		_pos.x = pos.x;
+		_pos.y = pos.y;
+		transform.position = _pos;
+		rigidbody.constraints = locked;
+		isLock = true;
+		transform.parent=null;
+	}
+	
+	public void FreeHand ()
+	{
+		rigidbody.constraints = free;
+		isLock = false;
+		transform.parent=bodyT;
+	}
+	
+	public void SetSpring (float spring)
+	{
+		this.spring = spring;
+	}
+	
+	public void SetDamper (float damper)
+	{
+		this.damper = damper;
+	}
+	
+	public bool IsLink{ get; set; }
+	
+	void OnMouseDown ()
+	{
+		if (isLock)
+			IsLink = !IsLink;
+	}
+	
+}
