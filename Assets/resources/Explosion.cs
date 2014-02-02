7e4c1c85f5cb94d3b3d12178cb9a9a0c3d72af1b
Cabris <christtabris@gmail.com>
2014-02-02 22:47:42 +0800

    explosion

---
+++ Assets/resources/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+
+		public GameObject button;
+
+		// Use this for initialization
+		void Start ()
+		{
+				UIEventListener.Get (button).onClick += ButtonClick;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void ButtonClick (GameObject button)
+		{
+				Debug.Log ("GameObject " + button.name);
+
+		}
+
+}
